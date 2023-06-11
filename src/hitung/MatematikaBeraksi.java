cda8b3e040a4cc987d8cc0dc5834bb9245d2b168
adhyharyadiii <mhadhyhrd@gmail.com>
2023-06-11 22:26:17 +0800

    Membuat class MatematikaBeraksi

--- src/hitung/MatematikaBeraksi.java
+++ src/hitung/MatematikaBeraksi.java
@@ -1,5 +1,14 @@
 package hitung;
 
 public class MatematikaBeraksi {
-    
+    public static void main(String[] args) {
+        //membuat objek
+        Matematika adhy = new Matematika(5,7);
+        
+        System.out.println("Hasil penjumlahan: "+adhy.setPenjumlahan());
+        System.out.println("Hasil pengurangan: "+adhy.setPengurangan());
+        System.out.println("Hasil perkalian: "+adhy.setPerkalian());
+        System.out.println("Hasil pembagian: "+adhy.setPembagian());
+       
+    }
 }
